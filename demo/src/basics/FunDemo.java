0f3a0662e70fdebc9e220f652241aad29fcad6d2
srikanthpragada <srikanthpragada@gmail.com>
2022-07-04 18:57:11 +0530

    03-JUL

---
+++ demo/src/basics/FunDemo.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class FunDemo {
+
+	public static int add(int n1, int n2) {
+		return n1 + n2;
+	}
+
+	public static int total(int...values) {
+		int sum = 0;
+		for (int n : values)
+			sum += n;
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(add(10, 10));
+		System.out.println(total(10, 20, 30));
+		System.out.println(total(10, 20, 30, 1, 3));
+
+	}
+
+}
