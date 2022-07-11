77e7bc8fb82025b93bb9adf06c3e3053537b91ed
srikanthpragada <srikanthpragada@gmail.com>
2022-07-11 18:57:39 +0530

    11-JUL

---
+++ demo/src/oop/TestFinal.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class TestFinal {
+    public final int a = 50;
+    
+    public TestFinal() {
+    	//a = 100;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- demo/src/oop/TestPerson.java
+++ demo/src/oop/TestPerson.java
@@ -1,20 +1,24 @@
 package oop;
 
 class Person {
+	private static int count = 0;
 	public Person(String name) {
-		
+		Person.count++;
+	}
+	public static int getCount() {
+		return Person.count;
 	}
 }
 
 public class TestPerson {
-
 	public static void main(String[] args) {
 		Person p1 = new Person("Abc");
 		Person people[] = new Person[5];
-		
-		//people[0] = new Person("Xyz");
+
+		people[0] = new Person("Xyz");
 		people[0].toString();
-		
+
+		System.out.println(Person.getCount());
 
 	}
 
