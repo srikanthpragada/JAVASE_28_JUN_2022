1bac3f58357a8cff35b3c69943222aac4e173143
srikanthpragada <srikanthpragada@gmail.com>
2022-07-29 18:55:35 +0530

    29-JUL

---
+++ demo/src/io/WriteAlpha.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class WriteAlpha {
+
+	public static void main(String[] args) throws Exception {
+		
+		File f = new File("d:\\classroom\\jun28\\alpha.txt");
+		if(f.exists()) {
+		   System.out.println("File is existing!");
+           return;
+		}
+		
+		try (FileWriter fw = new FileWriter(f)) {
+			for (int ch = 65; ch <= 90; ch++)
+				fw.write(ch);
+		}
+	}
+}
